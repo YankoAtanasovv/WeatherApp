5e7500532a6b2752e8af3b422d5df7e87508477f
YankoAtanasovv <btrouble39@gmail.com>
2024-02-02 11:35:45 +0200

    Search components Created

---
+++ weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "46858d4b4amshb14630f11145180p1cb748jsn155824fef00a",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
